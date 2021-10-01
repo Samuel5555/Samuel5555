61825ef48d13b7ebc631fdb2b2a23c1568c6d652
SammyStax <57230694+Samuel5555@users.noreply.github.com>
2021-10-01 07:50:18 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,5 @@
-// import firebase from "firebase/app";
-// import "firebase/firestore";
-
-// // Your web app's Firebase configuration
-// let firebaseConfig = {
-// 	apiKey: "AIzaSyBmbSg2xXXo4fKdc_zfBrtK-ZHDFOk1cV0",
-// 	authDomain: "sveltefire-test-b7129.firebaseapp.com",
-// 	projectId: "sveltefire-test-b7129",
-// 	storageBucket: "sveltefire-test-b7129.appspot.com",
-// 	messagingSenderId: "1022611496294",
-// 	appId: "1:1022611496294:web:8ca2ac4ae2f9baaa22fca7",
-// 	measurementId: "G-B95TGXN9LB",
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// export const database = firebase.firestore();
-// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: "AIzaSyD9pN_V9e3SS15DQ1OT3lXf9VeWaMJwQvk",
   authDomain: "davidibiyeomie59.firebaseapp.com",
   projectId: "davidibiyeomie59",
@@ -36,3 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+export const database = firebase.firestore();
